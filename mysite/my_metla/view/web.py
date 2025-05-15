aa157210d860eabf648e6b29558338f26937c1d2
stravkin <travkin-sergei1990@yandex.ru>
2025-05-16 00:32:26 +0300

    update Template

--- mysite/my_metla/view/web.py
+++ mysite/my_metla/view/web.py
@@ -3,10 +3,10 @@ from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView, DetailView
 from django_filters.views import FilterView
-from django.db.models import Count
+from django.db.models import Count, Prefetch
 
 from ..filters import BaseFilter, SchemaTableFilter, BaseSchemaFilter
-from ..models import Base, SchemaTable, BaseSchema, TableColumn
+from ..models import Base, SchemaTable, BaseSchema, TableColumn, TableColumnEnvironment
 
 
 class AboutAppView(TemplateView):
@@ -79,40 +79,6 @@ class BaseSchemaListView(FilterView):
         return context
 
 
-class TableDetailView(DetailView):
-    """Детализация таблицы с отображением сред разработки для столбцов."""
-
-    model = SchemaTable
-    template_name = 'my_metla/table-detail.html'
-    context_object_name = 'schema_table'
-    paginate_by = 20
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        # Получаем все столбцы для этой таблицы с предварительной загрузкой связанных данных
-        columns = TableColumn.objects.filter(
-            schema_table=self.object
-        ).select_related(
-            'column',
-            'column__type'
-        ).prefetch_related(
-            'tablecolumnenvironment_set__environment'
-        ).order_by('numbers')
-
-        # Собираем данные о средах разработки для каждого столбца
-        column_data = []
-        for column in columns:
-            environments = [env.environment for env in column.tablecolumnenvironment_set.all()]
-            column_data.append({
-                'column': column,
-                'environments': environments
-            })
-
-        context['column_data'] = column_data
-        return context
-
-
 class SchemaTableListView(FilterView):
     """Схема таблицы."""
 
@@ -142,4 +108,68 @@ class SchemaTableListView(FilterView):
         context = super().get_context_data(**kwargs)
         context['title'] = "Список связей База-Схема"
         context['filter'] = self.filterset
+        return context
+
+
+class TableDetailView(DetailView):
+    """Детализация таблицы с отображением сред разработки для столбцов."""
+    model = SchemaTable
+    template_name = 'my_metla/table-detail.html'
+    context_object_name = 'schema_table'
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Цветовая схема для сред разработки
+        ENVIRONMENT_COLORS = {
+            1: 'bg-secondary',
+            2: 'bg-info',
+            3: 'bg-primary',
+            4: 'bg-success',
+            5: 'bg-info text-dark',
+            6: 'bg-light text-dark',
+            7: 'bg-dark',
+        }
+
+        # Оптимизированный запрос с Prefetch
+        columns = TableColumn.objects.filter(
+            schema_table=self.object
+        ).select_related(
+            'column', 'column__type'
+        ).prefetch_related(
+            Prefetch(
+                'tablecolumnenvironment_set',
+                queryset=TableColumnEnvironment.objects.select_related('environment'),
+                to_attr='prefetched_envs'
+            )
+        ).order_by('numbers')
+
+        column_data = []
+        for column in columns:
+            # Получаем все существующие ID сред
+            env_ids = {env.environment.pk for env in column.prefetched_envs if env.environment}
+
+            processed_envs = []
+            if env_ids:
+                min_id, max_id = min(env_ids), max(env_ids)
+
+                # Заполняем диапазон от min до max
+                for env_id in range(min_id, max_id + 1):
+                    env = next((e.environment for e in column.prefetched_envs
+                                if e.environment and e.environment.pk == env_id), None)
+
+                    processed_envs.append({
+                        'pk': env_id,
+                        'name': env.name if env else None,
+                        'color': ENVIRONMENT_COLORS.get(env_id, 'bg-secondary'),
+                        'exists': env is not None
+                    })
+
+            column_data.append({
+                'column': column,
+                'environments': processed_envs if env_ids else None
+            })
+
+        context['column_data'] = column_data
         return context
