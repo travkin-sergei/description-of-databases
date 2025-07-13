f8b2f6c9a067c266b811498f8e595c80c3cf06c7
stravkin <travkin-sergei1990@yandex.ru>
2025-07-14 00:07:39 +0300

    update template and view updates

--- mysite/my_updates/views.py
+++ mysite/my_updates/views.py
@@ -1,4 +1,3 @@
-import json
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseNotFound
 from django.views import View
@@ -9,6 +8,7 @@ from django.views.generic import (
 )
 
 from .models import DimUpdateMethod, LinkUpdate
+from .filters import DimUpdateMethodFilter
 
 
 class PageNotFoundView(LoginRequiredMixin, View):
@@ -30,12 +30,23 @@ class AboutView(LoginRequiredMixin, TemplateView):
 
 
 class DimUpdateMethodView(LoginRequiredMixin, ListView):
-    """."""
+    """Список методов обновления."""
 
     model = DimUpdateMethod
     template_name = 'my_updates/updates.html'
     context_object_name = 'updates'
     paginate_by = 20
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = DimUpdateMethodFilter(self.request.GET, queryset=queryset)
+        return self.filterset.qs.order_by('name')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        context['title'] = "Методы обновления"
+        return context
 
 
 class DimUpdateMethodDetailView(LoginRequiredMixin, DetailView):
@@ -45,10 +56,21 @@ class DimUpdateMethodDetailView(LoginRequiredMixin, DetailView):
     template_name = 'my_updates/updates-detail.html'
     context_object_name = 'update_method'
 
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Добавляем связанные объекты LinkUpdate
-        context['related_links'] = LinkUpdate.objects.filter(name=self.object)
+        # Добавляем связанные объекты LinkUpdate с предварительной выборкой
+        context['related_links'] = LinkUpdate.objects.filter(
+            name=self.object
+        ).select_related(
+            'column__main__table__schema__base',
+            'column__main__table__schema',
+            'column__main__table',
+            'column__main',
+            'column__sub__table__schema__base',
+            'column__sub__table__schema',
+            'column__sub__table',
+            'column__sub',
+            'column__type'
+        )
         context['title'] = f"Метод обновления: {self.object.name}"
         return context
