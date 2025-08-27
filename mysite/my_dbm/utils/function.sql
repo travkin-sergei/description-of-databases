e8545991e26c0048f7383196f6a51959367c43f3
stravkin <travkin-sergei1990@yandex.ru>
2025-08-27 07:12:41 +0300

    add function.sql

---
+++ mysite/my_dbm/utils/function.sql
@@ -0,0 +1,100 @@
+CREATE OR REPLACE FUNCTION my_dbmatch.sync_database(stage_db text, name_db text)
+RETURNS void AS $$
+BEGIN
+    -- 1. Добавляем новые схемы
+    INSERT INTO my_dbmatch.link_base_schemas(created_at, updated_at, is_active, schema, description, base_id)
+    SELECT NOW(), NOW(), TRUE, ltd.schem_name, ltd.schem_description, ldb.base_id
+    FROM my_dbmatch.link_total_data AS ltd
+        LEFT JOIN my_dbmatch.dim_stage         AS dst ON dst.name = ltd.stage
+        LEFT JOIN my_dbmatch.link_db           AS ldb ON ldb.stage_id = dst.id AND ldb.alias = ltd.db_name
+        LEFT JOIN my_dbmatch.link_base_schemas AS lbs ON lbs.base_id = ldb.base_id AND lbs.schema = ltd.schem_name
+    WHERE lbs.id IS NULL
+      AND ltd.stage = stage_db
+      AND ltd.db_name = name_db;
+
+    -- 2. Таблицы
+    INSERT INTO my_dbmatch.link_tables(created_at, updated_at, is_active, is_metadata, name, description, schema_id, type_id)
+    SELECT NOW(), NOW(), TRUE, ltd.tab_is_metadata, ltd.tab_name, ltd.tab_description, lbs.id, dtt.id
+    FROM my_dbmatch.link_total_data AS ltd
+        LEFT JOIN my_dbmatch.dim_stage         AS dst ON dst.name = ltd.stage
+        LEFT JOIN my_dbmatch.link_db           AS ldb ON ldb.stage_id = dst.id AND ldb.alias = ltd.db_name
+        LEFT JOIN my_dbmatch.link_base_schemas AS lbs ON lbs.base_id = ldb.base_id AND lbs.schema = ltd.schem_name
+        LEFT JOIN my_dbmatch.dim_table_type    AS dtt ON dtt.name = ltd.tab_type
+        LEFT JOIN my_dbmatch.link_tables       AS lts ON lts.type_id = dtt.id AND lts.schema_id = lbs.id AND lts.name = ltd.tab_name
+    WHERE lts.id IS NULL
+      AND ltd.stage = stage_db
+      AND ltd.db_name = name_db;
+
+    -- 3. Колонки
+    INSERT INTO my_dbmatch.link_columns(
+        created_at, updated_at, is_active, date_create, type, columns, is_null, is_key, unique_together, "default", description, table_id)
+    SELECT NOW(), NOW(), TRUE,
+           ltd.col_date_create, ltd.col_type, ltd.col_columns, ltd.col_is_null, ltd.col_is_key,
+           ltd.col_unique_together, ltd.col_default, ltd.col_description, lts.id
+    FROM my_dbmatch.link_total_data AS ltd
+        LEFT JOIN my_dbmatch.dim_stage         AS dst ON dst.name = ltd.stage
+        LEFT JOIN my_dbmatch.link_db           AS ldb ON ldb.stage_id = dst.id AND ldb.alias = ltd.db_name
+        LEFT JOIN my_dbmatch.link_base_schemas AS lbs ON lbs.base_id = ldb.base_id AND lbs.schema = ltd.schem_name
+        LEFT JOIN my_dbmatch.dim_table_type    AS dtt ON dtt.name = ltd.tab_type
+        LEFT JOIN my_dbmatch.link_tables       AS lts ON lts.type_id = dtt.id AND lts.schema_id = lbs.id AND lts.name = ltd.tab_name
+        LEFT JOIN my_dbmatch.link_columns      AS lcs ON lcs.columns = ltd.col_columns AND lcs.table_id = lts.id
+    WHERE lcs.id IS NULL
+      AND ltd.stage = stage_db
+      AND ltd.db_name = name_db;
+
+    -- 4. Привязка колонок к stage
+    INSERT INTO my_dbmatch.link_columns_stage(created_at, updated_at, is_active, column_id, stage_id)
+    SELECT NOW(), NOW(), TRUE, lcs.id, dst.id
+    FROM my_dbmatch.link_total_data AS ltd
+        LEFT JOIN my_dbmatch.dim_stage          AS dst ON dst.name = ltd.stage
+        LEFT JOIN my_dbmatch.link_db            AS ldb ON ldb.stage_id = dst.id AND ldb.alias = ltd.db_name
+        LEFT JOIN my_dbmatch.link_base_schemas  AS lbs ON lbs.base_id = ldb.base_id AND lbs.schema = ltd.schem_name
+        LEFT JOIN my_dbmatch.dim_table_type     AS dtt ON dtt.name = ltd.tab_type
+        LEFT JOIN my_dbmatch.link_tables        AS lts ON lts.type_id = dtt.id AND lts.schema_id = lbs.id AND lts.name = ltd.tab_name
+        LEFT JOIN my_dbmatch.link_columns       AS lcs ON lcs.columns = ltd.col_columns AND lcs.table_id = lts.id
+        LEFT JOIN my_dbmatch.link_columns_stage AS lct ON lct.column_id = lcs.id AND lct.stage_id = dst.id
+    WHERE lct.id IS NULL
+      AND ltd.stage = stage_db
+      AND ltd.db_name = name_db;
+
+    -- 5. Деактивация лишних колонок
+    UPDATE my_dbmatch.link_columns_stage lct
+    SET is_active = FALSE
+    WHERE lct.stage_id = (SELECT id FROM my_dbmatch.dim_stage WHERE name = stage_db)
+      AND lct.column_id NOT IN (
+          SELECT lcs.id
+          FROM my_dbmatch.link_total_data ltd
+              JOIN my_dbmatch.link_db ldb ON ldb.alias = ltd.db_name
+              JOIN my_dbmatch.link_base_schemas lbs ON lbs.base_id = ldb.base_id AND lbs.schema = ltd.schem_name
+              JOIN my_dbmatch.link_tables lts ON lts.schema_id = lbs.id AND lts.name = ltd.tab_name
+              JOIN my_dbmatch.link_columns lcs ON lcs.table_id = lts.id AND lcs.columns = ltd.col_columns
+          WHERE ltd.stage = stage_db AND ltd.db_name = name_db
+      );
+
+    -- 6. Активация связанных
+    UPDATE my_dbmatch.link_columns_stage
+    SET is_active = TRUE
+    WHERE column_id IN (
+        SELECT lcs.id
+        FROM my_dbmatch.link_columns lcs
+            JOIN my_dbmatch.link_tables lts ON lts.id = lcs.table_id
+            JOIN my_dbmatch.link_base_schemas lbs ON lbs.id = lts.schema_id
+            JOIN my_dbmatch.link_db ldb ON ldb.base_id = lbs.base_id
+            JOIN my_dbmatch.dim_stage dst ON dst.id = ldb.stage_id
+        WHERE dst.name = stage_db AND ldb.alias = name_db
+    );
+
+    UPDATE my_dbmatch.link_columns
+    SET is_active = TRUE
+    WHERE id IN (SELECT column_id FROM my_dbmatch.link_columns_stage WHERE is_active);
+
+    UPDATE my_dbmatch.link_tables
+    SET is_active = TRUE
+    WHERE id IN (SELECT DISTINCT table_id FROM my_dbmatch.link_columns WHERE is_active);
+
+    UPDATE my_dbmatch.link_base_schemas
+    SET is_active = TRUE
+    WHERE id IN (SELECT DISTINCT schema_id FROM my_dbmatch.link_tables WHERE is_active);
+
+END;
+$$ LANGUAGE plpgsql;
