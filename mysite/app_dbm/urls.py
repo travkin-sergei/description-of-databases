778432eeca820220e532e2ac659bba4676b7d247
stravkin <travkin-sergei1990@yandex.ru>
2026-02-01 16:26:16 +0300

    update adb_dbm:url.py delete alt AJAX

--- mysite/app_dbm/urls.py
+++ mysite/app_dbm/urls.py
@@ -18,9 +18,6 @@ from .views.web import (
 
 # API-представления (DRF)
 from .views.v1 import (
-    SchemaByDBAPI,
-    TableBySchemaAPI,
-    ColumnByTableAPI,
     DimStageViewSet,
     DimDBViewSet,
     LinkDBViewSet,
@@ -50,23 +47,18 @@ router.register(r'total-data', TotalDataViewSet, basename='total-data')
 
 # === URL Patterns ===
 urlpatterns = [
-    # ——— Вспомогательные API (не через роутер) ———
-    path('api/v1/schemas/by-db/', SchemaByDBAPI.as_view(), name='schemas-by-db'),
-    path('api/v1/tables/by-schema/', TableBySchemaAPI.as_view(), name='tables-by-schema'),
-    path('api/v1/columns/by-table/', ColumnByTableAPI.as_view(), name='columns-by-table'),
 
-    # ——— Основной DRF API ———
+    # DRF AP
     path('api/v1/', include(router.urls)),
-
-    # ——— Веб-интерфейс (HTML) ———
+    # Для AJAX
+    path('api/schemas/', GetSchemasView.as_view(), name='get-schemas'),
+    path('api/tables/', GetTablesView.as_view(), name='get-tables'),
+    path('api/columns/', GetColumnsView.as_view(), name='get-columns'),
+    # HTML)
     path('about/', AboutView.as_view(), name='about-app'),
     path('', TablesView.as_view(), name='tables'),
     path('tables/<int:pk>/', TableDetailView.as_view(), name='tables-detail'),
     path('columns/', ColumnListView.as_view(), name='columns'),
     path('columns/<int:pk>/', ColumnDetailView.as_view(), name='columns-detail'),
     path('databases/', DatabasesView.as_view(), name='databases'),
-    # Для AJAX
-    path('api/schemas/', GetSchemasView.as_view(), name='get-schemas'),
-    path('api/tables/', GetTablesView.as_view(), name='get-tables'),
-    path('api/columns/', GetColumnsView.as_view(), name='get-columns'),
 ]
